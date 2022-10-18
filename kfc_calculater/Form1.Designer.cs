c6a2b919addc74876c90b0d9ac2684c5573dd28a
ColtenOuO <jun930436@gmail.com>
2022-10-19 03:49:00 +0800

    Version 1.0.0

---
+++ kfc_calculater/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace kfc_calculater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(414, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "開始計算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(22, 325);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(755, 103);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(546, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(230, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "清除所有資料";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(25, 225);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(507, 33);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "新增資料";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 199);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(137, 169);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(201, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(137, 143);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(201, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(137, 117);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(201, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(137, 91);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(201, 20);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "優惠代碼編號";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "內容物 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "內容物 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "內容物 3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(58, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "內容物 4 ";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(347, 119);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(45, 20);
+            this.textBox6.TabIndex = 14;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(347, 147);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(45, 20);
+            this.textBox7.TabIndex = 15;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(347, 173);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(45, 20);
+            this.textBox8.TabIndex = 16;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(347, 199);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(45, 20);
+            this.textBox9.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(349, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "滿足度 (如果沒有請填 0)";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(415, 199);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(117, 20);
+            this.textBox10.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(433, 180);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "此優惠的價格";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(544, 194);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(231, 24);
+            this.richTextBox2.TabIndex = 21;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(412, 146);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(219, 20);
+            this.textBox11.TabIndex = 22;
+            this.textBox11.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(412, 122);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(211, 13);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "如果可以重複最多只會選擇幾次本優惠";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(457, 292);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(139, 20);
+            this.textBox12.TabIndex = 24;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(515, 270);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(31, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "預算";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ kfc_calculater/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kfc_calculater
+{
+    public partial class Form1 : Form
+    {
+        public static int[,] dp = new int[105,1005];
+        public static int[] w = new int[1005];
+        public static int[] v = new int[1005];
+        public static string[] id = new string[1005];
+        public static int cnt = 1;
+        public static int[] last = new int[1005];
+        public void init()
+        {
+            for(int i=1;i<=100;i++)
+            {
+                for (int k = 0; k <= 1000; k++) dp[i, k] = 0;
+            }
+            richTextBox1.Text = "";
+            cnt = 1;
+
+            return;
+        }
+        public static void add(string s,int price,int happy)
+        {
+            id[cnt] = s;
+            w[cnt] = price;
+            v[cnt] = happy;
+            cnt++;
+            return;
+        }
+        public static bool check(string s)
+        {
+            for(int i=0;i<s.Length;i++)
+            {
+                if (s[i] >= '0' && s[i] <= '9') continue;
+                return false;
+            }
+
+            return true;
+        }
+        public void dynamic_programming(int num)
+        {
+            for(int i=1;i<cnt;i++)
+            {
+                for(int k=w[i];k<=num;k++)
+                {
+                    if (dp[i, k] < dp[i - 1, k - w[i]] + v[i]) dp[i, k] = dp[i - 1, k - w[i]] + v[i];
+                    else dp[i, k] = dp[i, k-1];
+                }
+            }
+
+            string ans = "";
+            int now = cnt - 1, have = dp[cnt - 1, num] , money = num;
+            ans += "選擇以下方案可以帶來的總總足度是 " + dp[cnt - 1, num].ToString() + "\n";
+            while( now != 0 )
+            {
+                for(int i=money;i>=w[now];i--)
+                {
+                    if( dp[now-1,i-w[now]] + v[now] == have )
+                    {
+                        ans += id[now];
+                        ans += "\n";
+                        have -= v[now];
+                        money = i - w[now];
+                        break;
+                    }
+                }
+
+                now--;
+            }
+
+            richTextBox1.Text = ans;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // 開始計算
+        {
+            string have = textBox12.Text;
+            bool ok7 = check(have);
+
+            if( ok7 == false )
+            {
+                richTextBox2.Text = "預算輸入有誤";
+            }
+            else
+            {
+                richTextBox2.Text = "";
+                dynamic_programming(Convert.ToInt32(have));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) // 清除資料
+        {
+            init();
+            richTextBox2.Text = "請輸入資訊";
+        }
+
+        private void button3_Click(object sender, EventArgs e) // 新增資料
+        {
+            string s1 = textBox1.Text, s2 = textBox2.Text, s3 = textBox3.Text , s4 = textBox4.Text;
+            string idx = textBox5.Text;
+
+            string same = textBox11.Text;
+
+            string price = textBox10.Text;
+
+            string happy1 = textBox9.Text;
+            string happy2 = textBox8.Text;
+            string happy3 = textBox7.Text;
+            string happy4 = textBox6.Text;
+
+
+            bool ok1 = check(happy1);
+            bool ok2 = check(happy2);
+            bool ok3 = check(happy3);
+            bool ok4 = check(happy4);
+            bool ok5 = check(price);
+            bool ok6 = check(same);
+
+            if (happy1 == "" || happy2 == "" || happy3 == "" || happy4 == "" || same == "" ) ok1 = false;
+            
+            if (ok1 == false || ok2 == false || ok3 == false || ok4 == false || ok5 == false || ok6 == false  )
+            {
+                richTextBox2.Text = "輸入有誤";
+            }
+            else
+            {
+                richTextBox2.Text = "新增成功";
+                int total = Convert.ToInt32(happy1) + Convert.ToInt32(happy2) + Convert.ToInt32(happy3) + Convert.ToInt32(happy4);
+                int need = Convert.ToInt32(price);
+
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox5.Text = "";
+                textBox6.Text = "";
+                textBox7.Text = "";
+                textBox8.Text = "";
+                textBox9.Text = "";
+                textBox10.Text = "";
+                textBox11.Text = "";
+                textBox12.Text = "";
+
+                for (int i = 0; i < Convert.ToInt32(same); i++) add(idx, need, total);
+            }
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
